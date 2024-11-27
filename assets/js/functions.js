109f32c9afe49d031b8889d84b1274cb867e58c6
August <augustinas.stas@gmail.com>
2024-11-27 20:09:31 +0200

    add

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,14 @@
-// Theme Toggle
 const themeToggle = document.getElementById('theme-toggle');
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
+  const icon = themeToggle.querySelector('i');
+  if (document.body.classList.contains('dark-mode')) {
+    icon.classList.remove('bi-sun');
+    icon.classList.add('bi-moon');
+  } else {
+    icon.classList.remove('bi-moon');
+    icon.classList.add('bi-sun');
+  }
 });
 
 // Anime.js Animation Example
@@ -19,17 +26,18 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('scroll', () => {
   const header = document.querySelector('.header');
   if (window.scrollY > 50) {
-    header.classList.add('scrolled'); // Add class to float and shrink header
+    header.classList.add('scrolled');
   } else {
-    header.classList.remove('scrolled'); // Remove class when back to top
+    header.classList.remove('scrolled');
   }
 });
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-  const navMenu = document.querySelector(".navmenu");
 
-  hamburgerMenu.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
+// Hamburger menu toggle
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.navmenu');
+
+  hamburgerMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
   });
 });
-
