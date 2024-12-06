b47de5aa1f00bb83bc449086520958b3ed9b8379
August <augustinas.stas@gmail.com>
2024-12-06 15:09:42 +0200

    add

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,28 +5,18 @@ themeToggle.addEventListener('click', () => {
 
   if (document.body.classList.contains('dark-mode')) {
     document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode'); // Add light mode
+    document.body.classList.add('light-mode'); // light mode
     icon.classList.remove('bi-moon');
     icon.classList.add('bi-sun');
   } else {
     document.body.classList.remove('light-mode');
-    document.body.classList.add('dark-mode'); // Add dark mode
+    document.body.classList.add('dark-mode'); // dark mode
     icon.classList.remove('bi-sun');
     icon.classList.add('bi-moon');
   }
 });
 
 
-// Anime.js Animation Example
-document.addEventListener('DOMContentLoaded', () => {
-  anime({
-    targets: '.animate-hero',
-    opacity: [0, 1],
-    translateY: [-50, 0],
-    duration: 2000,
-    easing: 'easeOutExpo',
-  });
-});
 
 // Add scroll listener to resize the header
 document.addEventListener('scroll', () => {
